92f19cff60cd9f0c81e4effb09a297794eb31137
michalinab <michalina.bartosik@gmail.com>
2016-07-06 15:50:44 +0200

    Pierwsze zajęcia

---
+++ src/main/java/com/lodz360/apka/MichalinaServlet.java
@@ -0,0 +1,25 @@
+package com.lodz360.apka;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by michalina on 06/07/16.
+ */
+public class MichalinaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.println("<bold>Michalina</bold>");
+
+
+
+
+    }
+}
