51c781061d00e18acc3e62cf2f4223172f836677
michalinab <michalina.bartosik@gmail.com>
2016-07-06 16:29:45 +0200

    Super zmiana

--- src/main/java/com/lodz360/apka/MichalinaServlet.java
+++ src/main/java/com/lodz360/apka/MichalinaServlet.java
@@ -16,7 +16,7 @@ public class MichalinaServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         ServletOutputStream outputStream = resp.getOutputStream();
-        outputStream.println("<bold>Michalina</bold>");
+        outputStream.println("<bold>Michalina Bartosik</bold>");
 
 
 
